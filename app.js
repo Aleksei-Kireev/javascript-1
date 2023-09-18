399f8036b18e75fd84d378946a41a84f24e8ed76
Aleksei-Kireev <alexey.kireev.1997@gmail.com>
2023-09-18 22:53:44 +0300

    Упражнения - Расчёт проекта

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-// Однострочный комментарий
-
 /*
-    Многострочный комментарий
+Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
+К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
+Выведете в консоль:
+- Boolean переменную успеете ли вы взяться за работу
+- Сколько вы за неё попросите?
 */
 
-const width = 10;
-const height = 5;
-const space = width * height;
-console.log('Площадь прямоугольника: ' + space);
+const priceToHour = 80;
+const hoursToDay = 5;
+const daysToWeek = 5;
+const work = 40;
+const daysToWeekend = 11;
 
-const city = 'Нижний Новгород';
-const street = 'Московская';
-console.log(city + ', ' + street + ' ' + 1);
+console.log((daysToWeekend - 2) * 5 >= work);
+console.log(work * priceToHour);
