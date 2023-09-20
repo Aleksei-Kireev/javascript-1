62f80ed70394f05c4449ddd6a5f350fcd2a555a1
Aleksei-Kireev <alexey.kireev.1997@gmail.com>
2023-09-20 21:06:50 +0300

    Упражнение - Размещение депозита

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const money = 100;
-const canBuy = money > 50;
+const moneyStart = 12000;
+const percent = 7;
+const months = 24;
+const price = 13500;
 
-if (canBuy) {
-    console.log('Может купить');
+const moneyEnd = moneyStart * (1 + percent / 100 / 12) ** months;
+
+if (moneyEnd > price) {
+    console.log('Дом куплен. Остаток: ' + (moneyEnd - price));
+} else if (moneyEnd == price) {
+    console.log('Дом куплен. Средств не осталось');
 } else {
-    console.log('Не хватает средств');
+    console.log('Не хватает средств: ' + (price - moneyEnd));
 }
-
-console.log('Конец программы');
