dc79297737dd14acf112dccfce5b3b6598fbffcd
Aleksei-Kireev <alexey.kireev.1997@gmail.com>
2023-10-08 13:57:09 +0300

    Упражнение - Кредит на MacBook

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-// Обычная функция
-function toPower(num, power) {
-    const res = num ** power;
-    return res;
-}
-console.log(toPower(2, 3));
+const userAge = 25;
+const userIsWork = true;
+const userMoney = 1500;
 
-// Стрелочная функция
-const toPowerArrow = (num, power) => num ** power;
-console.log(toPowerArrow(2, 3));
+const priceMacBook = 2000;
+
+function canBuy() {
+    if (userMoney >= priceMacBook) {
+        return 'Идем покупать ноутбук!';
+    } else if (bankMoney() + userMoney >= priceMacBook) {
+        return 'Идем покупать ноутбук!';
+    } else {
+        return 'Не хватает средств!';
+    }
+}
+
+function bankMoney() {
+    switch(true) {
+        case userAge > 24 && userIsWork === true:
+            return 500;
+        case userAge > 24:
+            return 100;
+        default:
+            return 0;
+    }
+}
+
+console.log(canBuy());
