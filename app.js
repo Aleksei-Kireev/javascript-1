85768df7c4c30d4e3f2770589b9f66757821d1e5
Aleksei-Kireev <alexey.kireev.1997@gmail.com>
2023-10-12 22:02:24 +0300

    Упражнение - Расчет итогового баланса

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const arr = ['!', 'JS', 'люблю', 'Я'];
-let arrNew = [];
+const operations = [1000, -700, 300, -500, 10000];
+let balance = 100;
+let len = operations.length;
+let positiveSum = 0, negativeSum = 0;
+let positiveIndex = 0, negativeIndex = 0;
 
-for (let i = 0; i < arr.length; i++) {
-    arrNew[arr.length - 1 - i] = arr[i];
+function getBalance(balance, operations) {
+    for (let i = 0; i < len; i++) {
+        if (balanceCheck(balance, operations[i])) {
+            balance += operations[i];
+            f3(operations[i]);
+        } else break;
+    }
+    return balance;
 }
 
-arrNew = arrNew.join(' ');
-console.log(arrNew);
+function balanceCheck(balance, num) {
+    if (balance + num >= 0) {
+        return true;
+    } else return false;
+}
+
+function f3(num) {
+    if (num >= 0) {
+        positiveSum += num;
+        positiveIndex++;
+    } else {
+        negativeSum += num;
+        negativeIndex++;
+    }
+}
+
+console.log(`Итоговый баланс: ${getBalance(balance, operations)}`);
+console.log(`Средний расход: ${negativeSum/negativeIndex}`);
+console.log(`Средний доход: ${positiveSum/positiveIndex}`);
