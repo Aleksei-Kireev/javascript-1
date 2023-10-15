f6caac7aeb8922a66d6c1b4d754a38e24a521c62
Aleksei-Kireev <alexey.kireev.1997@gmail.com>
2023-10-15 15:35:47 +0300

    Упражнение - Реализация some

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const arr = [1, 4, 4, 10];
+const arr = [2, 4, 4, 10, 20];
 
-const avg = arr.reduce((prev, current, index) => {
-    prev += current;
-    if (index === arr.length - 1) {
-        return prev / (index + 1);
-    } else {
-        return prev;
-    };
-}, 0);
-
-console.log(avg);
+console.log(arr.some(el => el === 3));
